7ef18e01fe474601552bede7cfa605b26c404822
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-10 20:14:47 +0800

    dynamic add macro to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sweetjs = require('gulp-sweetjs');
 var jasmine = require('gulp-jasmine');
+var fs = require('fs');
+
+var macros = fs.readdirSync('macro').map(function(m){return './macro/'+m})
 
 gulp.task('sweetify', function(){
   return gulp.src("spec/**/*.sjs")
     .pipe(sweetjs({
-      modules: ['./macro/lambda','./macro/ruspec','./macro/defn']
+      modules: macros
     }))
     .pipe(gulp.dest('spec/build'))
 
